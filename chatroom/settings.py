dc758290d99c568586537c9dcc7814129499566a
Harshavardhan Reddy <57401710+theh4cker1769@users.noreply.github.com>
2022-02-04 22:39:24 +0530

    DB updated

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -82,6 +82,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'connect',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
